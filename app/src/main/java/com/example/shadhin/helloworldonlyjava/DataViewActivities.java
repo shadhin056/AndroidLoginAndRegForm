a5e847051f09b9143c32f4e33ea47ebce04a6cc6
shadhin056 <shadhinemail@gmail.com>
2018-09-06 10:40:17 +0600

    required edit field added

--- app/src/main/java/com/example/shadhin/helloworldonlyjava/DataViewActivities.java
+++ app/src/main/java/com/example/shadhin/helloworldonlyjava/DataViewActivities.java
@@ -5,25 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class DataViewActivities extends AppCompatActivity{
-    TextView name;
-    TextView phone;
-    TextView bdaynow;
+public class DataViewActivities extends AppCompatActivity {
+    TextView nameView;
+    TextView phoneView;
+    TextView birthdayView;
+    TextView emailView;
+    TextView passwordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_data_view);
 
-        name=(TextView)findViewById(R.id.name_display);
-        phone=(TextView)findViewById(R.id.phone_display);
-        bdaynow=(TextView)findViewById(R.id.birthday_display_now);
-        String sessionId1= getIntent().getStringExtra("nick_name1");
-        String sessionId2= getIntent().getStringExtra("phone_number1");
-        String sessionId3= getIntent().getStringExtra("birthday1");
-        name.setText(sessionId1);
-        phone.setText(sessionId2);
-        bdaynow.setText(sessionId3);
+        nameView = (TextView) findViewById(R.id.name_display);
+        phoneView = (TextView) findViewById(R.id.phone_display);
+        birthdayView = (TextView) findViewById(R.id.birthday_display);
+        emailView = (TextView) findViewById(R.id.email);
+        passwordView = (TextView) findViewById(R.id.password);
+
+        String sessionId1 = getIntent().getStringExtra("nick_name1");
+        String sessionId2 = getIntent().getStringExtra("phone_number1");
+        String sessionId3 = getIntent().getStringExtra("birthday1");
+        String sessionId4 = getIntent().getStringExtra("email1");
+        String sessionId5 = getIntent().getStringExtra("password1");
+        String sessionId6 = getIntent().getStringExtra("repassword1");
+        nameView.setText(sessionId1);
+        phoneView.setText(sessionId2);
+        birthdayView.setText(sessionId3);
+        emailView.setText(sessionId4);
+        passwordView.setText(sessionId5);
 
     }
 }
--- app/src/main/java/com/example/shadhin/helloworldonlyjava/MainActivity.java
+++ app/src/main/java/com/example/shadhin/helloworldonlyjava/MainActivity.java
@@ -12,14 +12,19 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePickerDialog picker;
-    EditText nick_name;
-    EditText phone_number;
-    EditText birthday;
-    TextView birthday_day_text_view;
-    Button submit;
+    EditText nickName;
+    EditText phoneNumber;
+    EditText birthDay;
+    EditText email;
+    EditText password;
+    EditText rePassword;
+    TextView birthdayTextView;
+    Button submitButton;
 
 
     @Override
@@ -28,14 +33,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        nickName = findViewById(R.id.nick_name);
+        phoneNumber = findViewById(R.id.phone_numer);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        rePassword = findViewById(R.id.reenter_password);
+        birthdayTextView = findViewById(R.id.birthday_date_view);
+        birthDay = findViewById(R.id.birthday);
 
-        nick_name= findViewById(R.id.nick_name);
-        phone_number= findViewById(R.id.phone_numer);
-        birthday_day_text_view=findViewById(R.id.birthday_date_view);
-        birthday= findViewById(R.id.birthday);
-        birthday.setInputType(InputType.TYPE_NULL);
 
-        birthday.setOnClickListener(new View.OnClickListener() {
+        birthDay.setInputType(InputType.TYPE_NULL);
+
+        birthDay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
@@ -47,25 +56,28 @@ public class MainActivity extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                birthday.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                birthDay.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                             }
                         }, year, month, day);
                 picker.show();
             }
         });
-        submit=findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+        submitButton = findViewById(R.id.submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                birthday_day_text_view.setText("Selected Date: "+ birthday.getText());
-               // setContentView(R.layout.form_data_view);
-               Intent intent =new Intent(MainActivity.this,DataViewActivities.class);
+                birthdayTextView.setText("Selected Date: " + birthDay.getText());
+                // setContentView(R.layout.form_data_view);
+                Intent intent = new Intent(MainActivity.this, DataViewActivities.class);
 
-                intent.putExtra("nick_name1",nick_name.getText().toString());
-                intent.putExtra("phone_number1",phone_number.getText().toString());
-                intent.putExtra("birthday1",birthday.getText().toString());
+                intent.putExtra("nick_name1", nickName.getText().toString());
+                intent.putExtra("phone_number1", phoneNumber.getText().toString());
+                intent.putExtra("birthday1", birthDay.getText().toString());
+                intent.putExtra("email1", email.getText().toString());
+                intent.putExtra("password1", password.getText().toString());
+                intent.putExtra("repassword1", rePassword.getText().toString());
                 startActivity(intent);
             }
         });
     }
-    }
+}
